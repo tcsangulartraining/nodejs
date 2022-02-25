04148dd28265e89bd816de26d6fea50ce199b315
tcsangulartraining <tcsangulartraining@gmail.com>
2022-02-25 15:38:06 +0530

    Websocket implementation

--- server.js
+++ server.js
@@ -3,16 +3,24 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 var mongoose = require('mongoose');
+var socketIO = require('socket.io');
 var jwt = require('jsonwebtoken');
 var employeeModel = require("./source/models/employee");
 var auth = require("./source/middleware/auth");
+var WebSocketServer = require('websocket').server;
 
 var JWT_SECRET_KEY = 'ysmasd7a68764mllsdf834n';
+const PORT = 3000;
 
 // getting-started.js
 async function connectDB() {
   await mongoose.connect('mongodb://localhost:27017/TCS-Node');
 }
+
+//MongoDB Connection Initialization
+connectDB().catch(err => console.log(err));
+//Creating Http Server for Node
+const httpServer = http.createServer(app);
 
 /** Takes care of JSON data */
 app.use(express.json());
@@ -61,8 +69,21 @@ app.get('/listUsers', function (req, res) {
   res.send(token);
 });
 
-/** Server */
-connectDB().catch(err => console.log(err));
-const httpServer = http.createServer(app);
-const PORT = process.env.PORT ?? 3000;
+// Initialising Server in port 3000
 httpServer.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+
+//Mapping websocket with server
+const wsServer = new WebSocketServer({
+  httpServer: httpServer
+});
+
+wsServer.on('request', function(request) {
+  const connection = request.accept(null, request.origin);
+  connection.on('message', function(message) {
+    console.log('Received Message:', message.utf8Data);
+    connection.sendUTF('Hi this is WebSocket server!');
+  });
+  connection.on('close', function(reasonCode, description) {
+      console.log('Client has disconnected.');
+  });
+});
